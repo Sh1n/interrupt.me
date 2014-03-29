2c739c2b2398387309fbd480722139b31dd8387d
andreifiroiu <andrei.firoiu@gmail.com>
2014-03-29 20:49:05 +0100

    notification stuff

--- app/platforms/android/assets/www/js/index.js
+++ app/platforms/android/assets/www/js/index.js
@@ -16,120 +16,109 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+var APP_IS_ACTIVE = true;
+
+
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
+	// Application Constructor
+	initialize: function() {
+		this.bindEvents();
 
-        app.fbId = '750502531648621';
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+		app.fbId = '750502531648621';
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
 
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+	// deviceready Event Handler
+	//
+	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+	// function, we must explicity call 'app.receivedEvent(...);'
+	onDeviceReady: function()
+	{
+		app.receivedEvent('deviceready');
 
-        console.log('Received Event: ' + id);
+		document.addEventListener("pause", onPause, false);
+		document.addEventListener("resume", onResume, false);
 
-        //var serviceName = 'co.pouz.InterruptMeBgService';
-        //var factory = cordova.require('com.red_folder.phonegap.plugin.backgroundservice.BackgroundService');
-        //interruptBgService = factory.create(serviceName);
-        
-        //goBgService();
-    }
+
+		window.plugin.notification.local.onclick = function (json)
+		{
+			console.log("notification click event -- " + id);
+			console.log(json);
+
+			// go and display the interruptions
+		}
+	},
+
+	// Update DOM on a Received Event
+	receivedEvent: function(id)
+	{
+		var parentElement = document.getElementById(id);
+
+		console.log('Received Event: ' + id);
+	}
 };
 
 
-function goBgService() {
-    interruptBgService.getStatus(function(r){ startService(r); }, function(e){ displayError(e); });
-    //interruptBgService.registerForBootStart(function(r){ startService(r); }, function(e){ displayError(e); });
-};
 
-
-
-
-function getStatus()
+function onPause()
 {
-    interruptBgService.getStatus(function(r){ displayResult(r); }, function(e){ displayError(e); });
+	APP_IS_ACTIVE = false;
 }
 
 
-function displayResult(data)
+function onResume()
 {
-    alert("Is service running: " + data.ServiceRunning);
+	APP_IS_ACTIVE = true;
 }
 
 
-function displayError(data)
+function connectServer(pouzServer, userFbId)
 {
-    alert("We have an error");
+	pouzServer.registerInterruptionCallback(function(e)
+	{
+		//alert(e);
+		//goBgService();
+
+		//navigator.notification.vibrate(2000);
+		//navigator.notification.beep(1);
+
+		console.log("interruption recieved - " + JSON.stringify(e));
+
+		if (!APP_IS_ACTIVE)
+		{
+			var now = new Date().getTime();
+			var _almost_now = new Date(now + 2 * 1000);
+
+			window.plugin.notification.local.add({
+				//id:      1,
+				title:   e.label,
+				message: 'Time for an interruption!',
+				date:    _almost_now,
+
+				json:   JSON.stringify(e)
+			});
+
+			console.log("after notification");
+		}
+		else
+		{
+			// go directly and display the interruptions
+			alert('Interuption! ' + e.label);
+		}
+	});
+
+	pouzServer.openConnection(userFbId, '');
+
+	alert('connected');
+	console.log('server connected');
+	//pouzServer.interrupt("user2", "lunch");
 }
 
-
-function updateHandler(data)
-{
-    if (data.LatestResult != null)
-    {
-        try {
-            var resultMessage = document.getElementById("resultMessage");
-            resultMessage.innerHTML = data.LatestResult.Message;
-            
-            //navigator.notification.beep(1);
-            //navigator.notification.vibrate(2000);
-            alert('hey');
-            alert(data.LatestResult.Message);
-            
-            /*navigator.notification.alert(
-                'You are the winner!',  // message
-                alertDismissed,         // callback
-                'Game Over',            // title
-                'Done'                  // buttonName
-            );*/
-        }
-        catch (err) {}
-    }
-}
-
-
-
-function startService(data)
-{
-    if (data.ServiceRunning)
-        //enableTimer(data);
-        registerForUpdates(data);
-    else
-        //interruptBgService.startService(function(r){ enableTimer(r); }, function(e){ displayError(e); });
-        interruptBgService.startService(function(r){ registerForUpdates(r); }, function(e){ displayError(e); });
-}
-
-
-function enableTimer(data)
-{
-    if (data.TimerEnabled)
-        registerForUpdates(data);
-    else
-        interruptBgService.enableTimer(60000, function(r){ registerForUpdates(r); }, function(e){ displayError(e); });
-}
-
-
-function registerForUpdates(data)
-{
-    if (!data.RegisteredForUpdates)
-    {
-        interruptBgService.registerForUpdates(function(r){ updateHandler(r); }, function(e){ handleError(e); });
-    }
-}
 
--- app/www/js/login_controller.js
+++ app/www/js/login_controller.js
@@ -1,63 +1,18 @@
 pouzApp.controller('LoginController', ['$scope', 'pouzServer', 'FBConnection' , function($scope, pouzServer, FBConnection) {
 
-  $scope.login = function() {
+	$scope.login = function()
+	{
+		userFbId = "user1";
+		connectServer(pouzServer, userFbId);
 
-  	pouzServer.registerInterruptionCallback(function(e) {
-		//alert(e);
-		//goBgService();
-
-            	//navigator.notification.vibrate(2000);
-		//navigator.notification.beep(1);
-
-		console.log("interruption recieved - " + JSON.stringify(e));
-		
-		//window.plugin.notification.local.setDefaults({ autoCancel: true });
-		var now = new Date().getTime();
-		var _almost_now = new Date(now + 2 * 1000);
-
-		window.plugin.notification.local.add({
-			id:      1,
-			title:   'Reminder',
-			message: 'Dont forget to buy some flowers.',
-			//repeat:  'weekly',
-			date:    _almost_now,
-
-			foreground: foreground,
-			background: background
+	  	// DO LOGIN or GET USER's FB ID
+		/*   FBConnection.login(function() {
+			// open connection with server
+			connectServer(pouzServer, userFbId);
+			
+			// go to main view
+			$scope.setView('friends_list');
 		});
-		
-		function foreground(id) {
-			console.log('I WAS RUNNING ID='+id)
-		}
-
-		function background(id) {
-			console.log('I WAS IN THE BACKGROUND ID='+id)
-		}
-
-		console.log("after notification");
-	});
-
-	var userFbId = "user1";
-	pouzServer.openConnection(userFbId, '');
-
-	alert('connected');
-	console.log('server connected');
-	//pouzServer.interrupt("user2", "lunch");
-
-    //DO LOGIN or GET USER's FB ID
- /*   FBConnection.login(function() {
-      // open connection with server
-      pouzServer.registerInterruptionCallback(function(e) {
-      	alert(e);
-      });
-      var userFbId = "user1";
-      pouzServer.openConnection(userFbId, '');
-
-      // go to main view
-      $scope.setView('friends_list');
-    });
-*/
-  }
-
+		*/
+	}
 }]);
-
