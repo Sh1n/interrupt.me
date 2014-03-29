c25f48ccff63b09031c8ab607fde3349ac2fbded
Gyula Nemeth <gyula.nemeth85@gmail.com>
2014-03-29 12:25:54 +0100

    server with reactions.

--- server/server.js
+++ server/server.js
@@ -2,9 +2,36 @@ var io = require("socket.io").listen(3000);
 
 var sockets = {};
 
-var interruptionLabels = ["beer", "sex"];
-function randomInterruption() {
-	return interruptionLabels[Math.floor(Math.random()*interruptionLabels.length)];
+var interruptionLabels = [
+	{
+		label: "lunch",
+		time: {
+			from: 12,
+			to: 15
+		}
+	},
+	{
+		label: "beer",
+		time: {
+			from: 16,
+			to: 2
+		}
+	},
+	{
+		label: "sex",
+		time: {
+			from: 16,
+			to: 3
+		}
+	}
+];
+
+function sendMessage(fbId, messageName, messageObject) {
+	var socketArr = sockets[fbId];
+
+	for (var i = socketArr.length - 1; i >= 0; i--) {
+		socketArr[i].emit(messageName, messageObject);
+	}
 }
 
 
@@ -12,6 +39,7 @@ io.sockets.on("connection", function (socket) {
 	socket.on("token", function(data) {
 		if (data.fbId) {
 			socket.fbId = data.fbId;
+			socket.timeZoneOffset = data.timeZoneOffset;
 			sockets[data.fbId] = sockets[data.fbId] || [];
 			sockets[data.fbId].push(socket);
 		}
@@ -19,14 +47,20 @@ io.sockets.on("connection", function (socket) {
 
 	socket.on("interrupt", function (data) {
 		if (data.fbId && data.label) {
-			var socketArr = sockets[data.fbId];
+			sendMessage(data.fbId, "interruption", {
+				"label": data.label,
+				"senderFbId": socket.fbId
+			});
+		}
+	});
 
-			for (var i = socketArr.length - 1; i >= 0; i--) {
-				socketArr[i].emit("interruption", {
-					"label": data.label,
-					"sender": socket.fbId
-				});
-			}
+	socket.on("react", function(data) {
+		if (data.fbId && data.senderFbId && data.reaction && data.label) {
+			sendMessage(data.senderFbId, "reaction", {
+				"label": data.label,
+				"senderFbId": socket.fbId,
+				"reaction": data.reaction
+			});
 		}
 	});
 
@@ -36,7 +70,7 @@ io.sockets.on("connection", function (socket) {
 		if (!socketArr) {
 			return;
 		}
-		
+
 		for (var i = socketArr.length - 1; i >= 0; i--) {
 			if (socket === socketArr[i]) {
 				socketArr.splice(i, 1);
@@ -46,15 +80,27 @@ io.sockets.on("connection", function (socket) {
 	});
 });
 
+
+
+function filterForTime(element) {
+	var nowHours = new Date().getHours();
+	return nowHours >= element.time.from && nowHours < element.time.to;
+}
+
+function randomInterruption(elements) {
+	return elements[Math.floor(Math.random()*elements.length)];
+}
+
 setInterval(function() {
+
 	for (var prop in sockets) {
 		var socketArr = sockets[prop];
 
-		var label = randomInterruption();
+		var filteredLabels = interruptionLabels.filter(filterForTime);
+
+		var label = randomInterruption(filteredLabels);
 		for (var i = socketArr.length - 1; i >= 0; i--) {
-			socketArr[i].emit("interruption", {
-				"label": label
-			});
+			socketArr[i].emit("interruption", label.label);
 		}
 	}
 }, 1000 * 60);
