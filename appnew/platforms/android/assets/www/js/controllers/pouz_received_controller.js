daa0fd90ca2a2cb0e693fbb03d8ecfb3b12c2a0c
Lukáš Doležal <dolezal@itlab.cz>
2014-03-30 12:15:22 +0200

    fixed reaction and interruption views

--- appnew/platforms/android/assets/www/js/controllers/pouz_received_controller.js
+++ appnew/platforms/android/assets/www/js/controllers/pouz_received_controller.js
@@ -1,14 +1,17 @@
-pouzApp.controller('PouzReceivedController', ['$scope','interruptions', 'pouzServer', function($scope, interruptions, pouzServer) {
+pouzApp.controller('PouzReceivedController', ['$scope','interruptions', 'pouzServer', 'FBConnection', function($scope, interruptions, pouzServer, FBConnection) {
 
   $scope.interruption = interruptions.interruptions[$scope.view_params.label];
 
+  $scope.sender = FBConnection.get_friend($scope.view_params.senderFbId);
+
+
   $scope.accept = function() {
-    pouzServer.sendReaction($scope.view_params, true);
+    pouzServer.react($scope.view_params, true);
     $scope.setView('friends_list');
   }
 
   $scope.deny = function() {
-    pouzServer.sendReaction($scope.view_params, false);
+    pouzServer.react($scope.view_params, false);
     $scope.setView('friends_list');
   }
 
